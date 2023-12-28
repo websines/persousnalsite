6410d259eaa2ff0229148f4130dce043ea21c77f
websines <to.subh03@gmail.com>
2023-12-28 20:53:06 +0530

    light-only

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body className={`${inter.className} bg-gray-100`}>
         <TopBar />
         <Navbar />
